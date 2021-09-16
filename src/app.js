9a745a7e6c5a5cc92d4dfb2003fe485b3fea5c3d
tanktejas <tejastank20001@gmail.com>
2021-09-16 19:26:20 +0530

    add files

--- src/app.js
+++ src/app.js
@@ -5,15 +5,20 @@ const path=require('path');
 const port=process.env.PORT || 3000;
 
 // const path1=path.join(__dirname,'../views');   
-// exp.use(express.static(path1));   
-exp.set('view engine','hbs');   
-exp.set("views", path.join(__dirname, "../views")); 
- 
+// exp.use(express.static(path1));     
+// exp.set('view engine','hbs');   
+
+const path1=path.join(__dirname,'../views/');
+exp.use(express.static(path1));
+
+
+// exp.set("views", path.join(__dirname, "../views")); 
+     
 exp.get("/",(req,res)=>{ 
-       res.render('web');       
+       res.sendFile(path.join(__dirname,'../views/web.html'));        
 });      
        
 exp.listen(port,()=>{
     console.log("success...");
 });
- 
+         
